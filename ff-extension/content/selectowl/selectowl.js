7a32dd3360bc23c76c0ff1f6e8d486af4b032a4d
kub1x <j.podlaha@gmail.com>
2014-04-23 21:25:14 +0200

    initial commit of ff selectowl extension

---
+++ ff-extension/content/selectowl/selectowl.js
@@ -0,0 +1,55 @@
+
+
+var selectowl = { }
+
+
+selectowl.init = function () {
+  this.jOWL = window.jOWL;
+  this.load();
+}
+
+
+selectowl.load = function () {
+  var url = 'http://xmlns.com/foaf/spec/index.rdf';
+  //var url = $('#ontology-url').val();
+
+  if (!url) throw "empty url, nothing to load";
+
+  alert('loading: ' + url);
+
+  var callback = function () {
+    alert('loaded');
+    selectowl.refreshList();
+  };
+
+  this.jOWL.load(url, callback, {reason: true, locale: 'en'});
+}
+
+
+selectowl.refreshList = function() {
+  
+  var showOnClick = function ( event ) {
+    //TODO populate popup and show it on click on it or something 
+    alert(event.target.owlObj);
+  };
+
+  var list = $('#ontology-resources-list');
+  var idx = this.jOWL.index('ID');
+  var $listitem;
+  var p, q;
+
+  for ( p in idx ) {
+    q = idx[p];
+    // create listitem tag with all the functionality ;)
+    $listitem = $('<listitem />');
+    $listitem.attr('label', q.name + '[' + q.type + ']');
+    //$listitem.val(q.name + '[' + q.type + ']');
+    $listitem.get(0).owlObj = q;
+    $listitem.click(showOnClick);
+    // crate a list for it
+    list.append($listitem);
+    //t += p + ": " + idx[p] + '\n';
+  } 
+
+}
+
