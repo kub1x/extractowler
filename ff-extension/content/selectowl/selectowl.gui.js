3e44ef289812142bd5adbae6928fc61ceb69736f
kub1x <j.podlaha@gmail.com>
2014-04-26 22:27:51 +0200

    added dynamically generated workflow

--- ff-extension/content/selectowl/selectowl.gui.js
+++ ff-extension/content/selectowl/selectowl.gui.js
@@ -1,12 +1,76 @@
 
-selectowl.gui.showStep = function (step_id ) {
-  var $curr = $('.' + this.CURRENT_STEP_CLASS);
-  var $next = $('#' + step_id);
+selectowl.gui.init = function () {
+  selectowl.gui.populateWorkflow();
+}
 
-  // hide current
-  $curr.hide({ effect: "slide", "direction": "left" }, 500, function() {
-    // then show next
-    $next.show({ effect: "slide", "direction": "right" }, 500);
+selectowl.gui.populateWorkflow = function () {
+  // Create following structure: 
+  //<hbox id="workflow">
+  //  <label id="step-link-load-ontology"  value="Load Ontology" onclick="onStepLinkClick(event);" />
+  //  <splitter />
+  //  <label id="step-link-select-action"  value="Select Action" onclick="onStepLinkClick(event);" />
+  //  <splitter />
+  //  <vbox>
+  //    <label id="step-link-select-object"  value="Select Object"          onclick="onStepLinkClick(event);" />
+  //    <label id="step-link-select-objprop" value="Select Object Property" onclick="onStepLinkClick(event);" />
+  //  </vbox>
+  //</hbox>
+
+  var onStepLinkClick = function( event ) {
+    // Get group_id
+    var $l = $(event.target);
+    var id = $l.attr('id');
+    var group_id = id.replace(selectowl.gui.WORKFLOW_LINK_PREFIX, '');
+    // Select it
+    selectowl.gui.showStep(group_id);
+  }
+
+  var $wf = $('hbox#workflow');
+  var $groups = $('#main').children('groupbox');
+
+  // Clear worflow menu
+  $wf.empty();
+
+  $groups.each( function(index, element) {
+    debugger;
+    var $grp = $(this);
+    var $lab = $('<label />');
+    // set label id
+    $lab.attr('id', selectowl.gui.WORKFLOW_LINK_PREFIX + $grp.attr('id'));
+    // set label text
+    $lab.attr('value', $grp.children('caption').attr('label'));
+    // show the active one
+    if ( $grp.hasClass(selectowl.gui.CURRENT_STEP_CLASS) ) {
+      $lab.addClass(selectowl.gui.CURRENT_STEP_CLASS);
+    }
+    // change active group on click
+    $lab.click(onStepLinkClick);
+    // actually add it
+    $wf.append($lab);
+  });
+
+
+}
+
+selectowl.gui.showStep = function (step_id ) {
+  var $currgrp = $('groupbox' + '.' + selectowl.gui.CURRENT_STEP_CLASS);
+  var $nextgrp = $('groupbox' + '#' + step_id);
+
+  var $currlab = $('label' + '.' + selectowl.gui.CURRENT_STEP_CLASS);
+  var $nextlab = $('label' + '#' + step_id);
+
+  // Apply imediately for labels...
+  $currlab.removeClass(selectowl.gui.CURRENT_STEP_CLASS);
+  $nextlab.addClass(selectowl.gui.CURRENT_STEP_CLASS);
+
+  // Apply imediately for groups
+  $currgrp.removeClass(selectowl.gui.CURRENT_STEP_CLASS);
+  $nextgrp.addClass(selectowl.gui.CURRENT_STEP_CLASS);
+
+  // Start animation - hide current
+  $currgrp.hide({ effect: "slide", "direction": "left" }, 500, function() {
+    // Followed by - show next
+    $nextgrp.show({ effect: "fade" }, 500);
   });
 
   //// hide current
@@ -15,6 +79,4 @@ selectowl.gui.showStep = function (step_id ) {
   //  $next.effect("slide", { "direction": "right", "mode": "show" }, 500);
   //});
 
-  $curr.removeClass(this.CURRENT_STEP_CLASS);
-  $next.addClass(this.CURRENT_STEP_CLASS);
 }
--- ff-extension/content/selectowl/selectowl.js
+++ ff-extension/content/selectowl/selectowl.js
@@ -4,7 +4,8 @@ var selectowl = {
   }, 
 
   gui : {
-    CURRENT_STEP_CLASS : "current-step"
+    CURRENT_STEP_CLASS : "current-step", //TODO load from prefs
+    WORKFLOW_LINK_PREFIX : 'wf-link-',   //TODO load from prefs
   }, 
 
   workflow : {
@@ -18,23 +19,32 @@ var selectowl = {
 }
 
 
+selectowl.init = function () {
+  selectowl.gui.init();
+  selectowl.load(); //TODO debug only - delete
+}
+
+
 selectowl.load = function (url) {
-  console.log('url: ' + url + ', !url: ' + !url);
+  //console.log('loading url: ' + url + ', !url: ' + !url);
   if (!url) { url =  $('#ontology-url').val(); }
+
   //TODO remove default to foaf (testing only)
   if (!url) url = 'http://xmlns.com/foaf/spec/index.rdf';
+
   //TODO put back the "empty url" exception
   //if (!url) { throw "empty url, nothing to load"; }
 
   // Call load on jOWL and refresh after
   var callback = function () {
+    //TODO load to selectowl and delete jOWL
+    //selectowl.feedMee(jOWL.index('ID'));
     selectowl.refreshList();
     selectowl.gui.showStep('select-action');
   };
 
   jOWL.load(url, callback, {reason: true, locale: 'en'});
 }
-
 
 
 selectowl.refreshList = function() {
